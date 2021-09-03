20cbf2d8c0b8d41c0c2c7ea66cd1bb625529241a
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-03 20:51:05 -0300

    Criado controller rota de atualização (unidades)

--- src/modulos/unidades/controlador/ControladorUnidade.js
+++ src/modulos/unidades/controlador/ControladorUnidade.js
@@ -47,5 +47,46 @@ module.exports = {
     })
 
     return res.status(201).send()
+  },
+
+  async atualizar(req, res) {
+    const { id } = req.params;
+
+    try {
+      const unidade = await knex('unidades').where({ id })
+      if (!unidade.length > 0) {
+        return res.status(400).json({ message: "Não foi encontrada nenhuma unidade com este identificador." })
+      }
+    } catch (err) {
+      return res.status(400).json({ message: "Não foi encontrada nenhuma unidade com este identificador." })
+    }
+
+    const {
+      cep,
+      rua,
+      complemento,
+      numero,
+      bairro,
+      cidade,
+      estado,
+      capacidade_total,
+      capacidade_reduzida,
+    } = req.body;
+
+    await knex('unidades')
+      .where({ id })
+      .update({
+        cep,
+        rua,
+        complemento,
+        numero,
+        bairro,
+        cidade,
+        estado,
+        capacidade_total,
+        capacidade_reduzida,
+      })
+
+    return res.status(204).send()
   }
 }
--- src/rotas.js
+++ src/rotas.js
@@ -5,5 +5,7 @@ const controladorUnidade = require('./modulos/unidades/controlador/ControladorUn
 
 rotas.get('/unidades', controladorUnidade.listar)
 rotas.post('/unidades', controladorUnidade.criar)
+rotas.put('/unidades/:id', controladorUnidade.atualizar)
+rotas.delete('/unidades/:id', controladorUnidade.apagar)
 
 module.exports = rotas;
