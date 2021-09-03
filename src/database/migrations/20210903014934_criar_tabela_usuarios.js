ff93787978f0917399ca18014c113392bdaec9e1
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-03 12:36:38 -0300

    Criado migration de consultores

--- src/database/migrations/20210903014934_criar_tabela_usuarios.js
+++ src/database/migrations/20210903014934_criar_tabela_usuarios.js
@@ -1,7 +1,11 @@
 exports.up = knex =>
-  knex.schema.createTable('users', table => {
-    table.uuid('id')
-    table.text('name').unique().notNullable()
+  knex.schema.createTable('consultores', table => {
+    table.uuid('id').primary()
+    table.text('nome').notNullable()
+    table.text('nome_social').notNullable()
+    table.text('imagem')
+    table.text('email').notNullable()
+    table.text('senha').notNullable()
   });
 
-exports.down = knex => knex.schema.dropTable('users');
+exports.down = knex => knex.schema.dropTable('consultores');
