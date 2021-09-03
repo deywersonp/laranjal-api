d2b622220b680e073aec7e0a2437c500b6ed6254
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-03 16:35:43 -0300

    Removido notNull nome social / Adicionado coluna espaco_agendado

--- src/database/migrations/20210903014934_criar_tabela_usuarios.js
+++ src/database/migrations/20210903014934_criar_tabela_usuarios.js
@@ -2,10 +2,11 @@ exports.up = knex =>
   knex.schema.createTable('consultores', table => {
     table.uuid('id').primary()
     table.text('nome').notNullable()
-    table.text('nome_social').notNullable()
+    table.text('nome_social')
     table.text('imagem')
     table.text('email').notNullable()
     table.text('senha').notNullable()
+    table.text('espaco_agendado').notNullable()
     table.boolean('admin').notNullable()
   });
 
