30c3e9c7145a2184055dcce83b59f9f4400ed5bf
Bruno Sousa <brunodsousa@outlook.com>
2021-09-03 22:25:41 -0300

    Adicionando filtro de autenticação

--- src/rotas.js
+++ src/rotas.js
@@ -2,8 +2,12 @@ const express = require("express");
 const rotas = express();
 const cadastrarConsultor = require("./modulos/consultores/cadastrarConsultor");
 const loginConsultor = require("./modulos/consultores/login");
+const verificarToken = require("./filtros/verificarToken");
 
 rotas.post("/consultores", cadastrarConsultor);
 rotas.post("/login", loginConsultor);
+
+rotas.use(verificarToken);
+
 
 module.exports = rotas;
