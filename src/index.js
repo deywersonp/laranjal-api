a2f16c3efcf253e167a137af68d75c662ff2a380
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-09-16 22:02:02 -0300

    Finalizado documentação da aplicação no swagger

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ require("dotenv").config();
 
 const express = require("express");
 const rotas = require("./rotas");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("../swagger.json");
+
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '30mb' }));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(rotas);
 
 app.listen(8000, () => console.log("Server is running!"));
